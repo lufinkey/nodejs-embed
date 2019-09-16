96b74d0def2c7db6353b0dd9d64d453044e56c62
Luis Finke <lfinke@fusionalliance.com>
2019-09-15 22:11:38 -0400

    build_android.sh script for nodejs

---
+++ external/nodejs/build_android.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# enter script directory
+base_dir=$(dirname "${BASH_SOURCE[0]}")
+cd "$base_dir"
+base_dir="$PWD"
+build_path="build/mobile"
+
+# build headers and clone repo
+./build_mobile_headers.sh || exit $?
+
+# clone NDK if needed
+if [ -z "$ANDROID_HOME" ] || [ ! -d "$ANDROID_HOME" ] || [ ! -d "$ANDROID_HOME/ndk-bundle" ]; then
+	cd "$base_dir/$build_path"
+	ndk_url="https://dl.google.com/android/repository/android-ndk-r18b-darwin-x86_64.zip"
+	if [ -n "$(which curl)" ]; then
+		curl "$ndk_url" -o "android_ndk.zip" || exit $?
+	else
+		wget "$ndk_url" -O "android_ndk.zip" || exit $?
+	fi
+	unzip "android_ndk.zip" || exit $?
+	NDK_DIR="$PWD/android-ndk-r18b"
+else
+	NDK_DIR="$ANDROID_HOME/ndk-bundle"
+fi
+
+# build node for android
+cd "$base_dir/$build_path/node" || exit $?
+./tools/android_build.sh "$NDK_DIR" || exit $?
+
+# copy built library to output dir
+cd "$base_dir/$build_path" || exit $?
+mkdir -p "android" || exit $?
+cp -f "node/out/Release/lib.target/libnode.so" "android/libnode.so" || exit $?
