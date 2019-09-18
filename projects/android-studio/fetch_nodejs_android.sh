7e933fdeb74ac1063b745906b38e79f4f48da7b7
Luis Finke <lfinke@fusionalliance.com>
2019-09-18 01:14:44 -0400

    Ensure we're in base dir in fetch_nodejs_android.sh

--- projects/android-studio/fetch_nodejs_android.sh
+++ projects/android-studio/fetch_nodejs_android.sh
@@ -7,9 +7,9 @@ base_dir="$PWD"
 build_path="libnodejsembed/build"
 source_path="libnodejsembed/src/main"
 
-# ensure we don't rebuild if we don't need to
 if [ ! -f "$base_dir/$build_path/libnode/bin/x86/libnode.so" ]; then
-	mkdir -p "$build_path" && cd "$build_path" || exit $?
+	# Download NodeJS Mobile distribution
+	cd "$base_dir" && mkdir -p "$build_path" && cd "$build_path" || exit $?
 	rm -rf "libnode" && mkdir "libnode" && cd "libnode" || exit $?
 	nodejs_mobile_url="https://github.com/JaneaSystems/nodejs-mobile/releases/download/nodejs-mobile-v0.2.1/nodejs-mobile-v0.2.1-android.zip"
 	if [ -n "$(which curl)" ]; then
@@ -21,6 +21,7 @@ if [ ! -f "$base_dir/$build_path/libnode/bin/x86/libnode.so" ]; then
 	rm -rf "nodejs-mobile-android.zip"
 fi
 if [ ! -f "$base_dir/$source_path/jniLibs/x86/libnode.so" ]; then
+	# Copy NodeJS Mobile libs
 	cd "$base_dir/$source_path" || exit $?
 	mkdir -p "jniLibs" || exit $?
 	cd "$base_dir/$build_path/libnode/bin" || exit $?
